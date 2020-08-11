6c57ea5138904a2c7525f74eeeed62476cf3a7d9
arjunbhatt670 <47890778+arjunbhatt670@users.noreply.github.com>
2020-08-11 17:24:33 +0530

    Create number of zeroes.java

---
+++ number of zeroes.java
@@ -0,0 +1,14 @@
+     ...  // Write a program that calculates the number of zeros from the back to the first non-zero number in N!
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		int n = new Scanner(System.in).nextInt();
+		
+		int cnt2 = 0, cnt5 = 0;
+		for(int i = 2; i <= n; i *= 2) cnt2 += n/i;
+		for(int i = 5; i <= n; i *= 5) cnt5 += n/i;
+
+		System.out.print(Math.min(cnt2, cnt5));
+	}
+}
